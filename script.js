377ec64fe33dbf25e0cd72767313d381b7adc4f8
Leny Mory <leny.mory@divtec.ch>
2024-09-24 14:38:41 +0200

    etapes 1-3

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 /**
  * Intro à vue.js
- * @author Steve Fallet
+ * @author Leny Mory
  * @date 2024-09-17
  */
+const {createApp, ref} = Vue;
 
-console.log('Intro à vue.js');
+createApp({
+    setup() {
+        const message = "Bienvenue dans PokeCount!";
+        const compteur = ref(0);
+        const capturesTab = ref([]);
+
+        function capturer() {
+            compteur.value += 1;
+        }
+
+        function sauver() {
+            if (compteur.value > 0) {
+                capturesTab.value.push(compteur.value);
+                compteur.value = 0;
+            }
+        }
+
+        return {
+            message,
+            compteur,
+            capturer,
+            sauver,
+
+        };
+    }
+
+}).mount("#poke-app");
